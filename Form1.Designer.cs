1c29611ae49c70bfbe034528bc1ef0918981a209
gchaim <gchaimke@gmail.com>
2020-03-17 11:25:56 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace C3_Logger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbIP = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txbLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbIP
+            // 
+            this.txbIP.Location = new System.Drawing.Point(12, 12);
+            this.txbIP.Name = "txbIP";
+            this.txbIP.Size = new System.Drawing.Size(100, 20);
+            this.txbIP.TabIndex = 0;
+            this.txbIP.Text = "192.168.2.46";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(118, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(105, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Get Log";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txbLog
+            // 
+            this.txbLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txbLog.Location = new System.Drawing.Point(0, 39);
+            this.txbLog.Multiline = true;
+            this.txbLog.Name = "txbLog";
+            this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbLog.Size = new System.Drawing.Size(348, 135);
+            this.txbLog.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "test";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 174);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbLog);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txbIP);
+            this.Name = "Form1";
+            this.Text = "C3-200 Get Log";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txbLog;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C3_Logger
+{
+    public partial class Form1 : Form
+    {
+        String documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string strcount = "";
+        IntPtr h = IntPtr.Zero;
+        Dictionary<int, String> users = new Dictionary<int, String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("plcommpro.dll", EntryPoint = "Connect")]
+        public static extern IntPtr Connect(string Parameters);
+
+        [DllImport("plcommpro.dll", EntryPoint = "PullLastError")]
+        public static extern int PullLastError();
+
+        [DllImport("plcommpro.dll", EntryPoint = "Disconnect")]
+        public static extern void Disconnect(IntPtr h);
+
+        [DllImport("plcommpro.dll", EntryPoint = "GetDeviceData")]
+        public static extern int GetDeviceData(IntPtr h, ref byte buffer, int buffersize, string tablename, string filename, string filter, string options);
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            Disconnect(h);
+            Cursor = Cursors.WaitCursor;
+            string str = "";
+            str = "protocol=TCP,ipaddress=" + this.txbIP.Text + ",port=4370,timeout=3000,passwd=";  //protocol=TCP,ipaddress=192.168.2.46,port=4370,timeout=2000,passwd=
+            if (IntPtr.Zero == h)
+            {
+                h = Connect(str);
+                
+                if (h != IntPtr.Zero)
+                {
+                    btnConnect.Enabled = false;
+                    txbLog.Text += "Connected!"+ Environment.NewLine;
+                    getLog();
+                    Disconnect(h);
+                    h = IntPtr.Zero;
+                    txbLog.Text += "Disconnected!"+ Environment.NewLine;
+                    btnConnect.Enabled = true;
+                    System.Diagnostics.Process.Start(documents);
+                }
+                else
+                {
+                    txbLog.Text += "Connection error!"+ Environment.NewLine;
+                }
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void getLog()
+        {
+            int ret = 0;
+            string str = "Cardno\tPin\tEventType\tInOutState\tTime_second";
+            int BUFFERSIZE = 1 * 1024 * 1024;
+            byte[] buffer = new byte[BUFFERSIZE];
+            string options = "";
+
+            if (IntPtr.Zero != h)
+            {
+                ret = GetDeviceData(h, ref buffer[0], BUFFERSIZE, "transaction", str, "EventType=207", options);
+            }
+            else
+            {
+                MessageBox.Show("Connect device failed!");
+                return;
+            }
+
+            if (ret >= 0)
+            {
+                strcount = Encoding.Default.GetString(buffer);
+                try
+                {
+                    StreamWriter sw = new StreamWriter(documents + "\\ESD_LOG.ROW");
+                    sw.WriteLine(strcount);
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: " + ex.Message);
+                    txbLog.Text += ex.Message + Environment.NewLine;
+                }
+                saveLog();
+                txbLog.Text +="Get " + ret + " records" + Environment.NewLine;
+            }
+            else
+            {
+                txbLog.Text += "Get data failed.The error is " + ret + Environment.NewLine;
+                return;
+            }
+        }
+
+        private String[] formatLog(String rowTime)
+        {
+            int second, minute, hour;
+            String strSecond, strMinute, strHour;
+            String[] date =new String[4]; //time,day,month,year
+            int time = Int32.Parse(rowTime);
+            second = time % 60;
+            if (second < 10)
+                strSecond = "0"+second;
+            else
+                strSecond = "" + second;
+            time = time / 60;
+            minute = time % 60;
+            if (minute < 10)
+                strMinute = "0" + minute;
+            else
+                strMinute = "" + minute;
+            time = time / 60;
+            hour = time % 24;
+            if (hour < 10)
+                strHour = "0" + hour;
+            else
+                strHour = "" + hour;
+
+            date[0] = String.Format("{0}:{1}:{2}", strHour, strMinute, strSecond);
+
+            time = time / 24;
+            date[1] = (time % 31 + 1)+"";
+
+            time = time / 31;
+            date[2] = (time % 12 + 1)+"";
+
+            time = time / 12;
+            date[3] = (time + 2000)+"";
+            return date;
+        }
+
+
+        private void saveLog()
+        {
+            try
+            {
+                getUserNames();
+            }
+            catch(Exception ex)
+            {
+                txbLog.Text += ex.Message + Environment.NewLine;
+            }
+
+            StreamReader sr = new StreamReader(documents + "\\ESD_LOG.ROW");
+            try
+            {
+                
+                StreamWriter sw = new StreamWriter(documents + "\\ESD_LOG.csv");
+                sw.WriteLine("Name,"+sr.ReadLine()+",Day,Month,Year");
+                while (sr.Peek() > 0)
+                {
+                    String[] arr = sr.ReadLine().Split(',');
+                    String[] date = formatLog(arr[4]);
+                    String line = "";
+                    if (users.Count > 0)
+                    {
+                        line = users[Int16.Parse(arr[1])] + "," + arr[0] + "," + arr[1] + "," + arr[2] + ",PASSED," + date[0]+","+ date[1] + "," + date[2] + "," + date[3];
+                    }
+                    else
+                    {
+                        txbLog.Text += "Fale with names not found, printing user number" + Environment.NewLine;
+                        line = arr[0] + "," + arr[1] + "," + arr[2] + ",PASSED," + date[0] + "," + date[1] + "," + date[2] + "," + date[3];
+                    }
+                    sw.WriteLine(line);
+                }
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                txbLog.Text += ex.Message + Environment.NewLine;
+            }
+            sr.Close();
+            delRow();
+        }
+
+        private void delRow()
+        {
+            try
+            {
+                // Check if file exists with its full path    
+                if (File.Exists(documents + "\\ESD_LOG.ROW"))
+                {
+                    // If file found, delete it    
+                    File.Delete(documents + "\\ESD_LOG.ROW");
+                    Console.WriteLine("File deleted.");
+                }
+                else Console.WriteLine("File not found");
+            }
+            catch (IOException ioExp)
+            {
+                Console.WriteLine(ioExp.Message);
+            }
+        }
+
+        private void buildHTML(int year, int month)
+        {
+            DateTimeFormatInfo dinfo = new DateTimeFormatInfo();
+            int nodays = DateTime.DaysInMonth(year, month);
+
+            StreamReader sr = new StreamReader(documents + "\\ESD_LOG.csv");
+            StreamWriter sw = new StreamWriter(documents + "\\ESD_LOG.html");
+
+            String page_start = "<!doctype html>\n <html lang = \"en\">\n<head><meta charset = \"utf-8\">\n" +
+                "<title> ESD Table</title>\n" +
+                "<meta name=\"description\"content=\"The HTML5 Herald\">\n" +
+                "<meta name=\"author\"content=\"SitePoint\">\n" +
+                "<link rel=\"stylesheet\" href=\"styles.css?v=1.0\"> </head><body>\n" +
+                "<script src=\"script.js\"></script>";
+
+            sw.WriteLine(page_start);
+            String table = "<table id='tbMonth'>";
+            String[] headLine = sr.ReadLine().Split(',');
+            getUserNames();
+            table += "<tr><th>ID<th> <th>User Name<th>";
+            for(int i = 0; i <= nodays; i++)
+            {
+                table += "<th>"+ i + "</th>";
+            }
+               
+            table += "</tr>";
+
+            foreach (KeyValuePair<int, string> user in users)
+            {
+                table += string.Format("<tr><td>{0}</td><td>{1}</td></tr>", user.Key, user.Value);
+            }
+
+            sw.WriteLine(table);
+
+            String page_end="</body></html>";
+            sw.WriteLine(page_end);
+
+            sr.Close();
+            sw.Close();
+            System.Diagnostics.Process.Start(documents);
+        }
+
+        private void getUserNames()
+        {
+            string mdfFile = @"C:\ZKTeco\ZKAccess3.5\access.mdb";
+
+            using (OleDbConnection connection = new OleDbConnection(string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", mdfFile)))
+            {
+                using (OleDbCommand selectCommand = new OleDbCommand("SELECT * FROM USERINFO", connection))
+                {
+                    connection.Open();
+
+                    DataTable table = new DataTable();
+                    OleDbDataAdapter adapter = new OleDbDataAdapter();
+                    adapter.SelectCommand = selectCommand;
+                    adapter.Fill(table);
+
+                    foreach (DataRow row in table.Rows)
+                    {
+                        object nameValue = row["NAME"];
+                        object userNum = row["Badgenumber"];
+
+                        users.Add(Int16.Parse(userNum.ToString()),nameValue.ToString());
+                    }
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //dbTest();
+            buildHTML(2020,03);
+        }
+    }
+}
