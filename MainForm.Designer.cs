535d80bf37f7adedc8b5de2ccd2c62ff8ff43ea2
chaim <gchaimke@gmail.com>
2020-03-18 15:02:13 +0200

    htnl month builder added

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -32,6 +32,7 @@
             this.txbLog = new System.Windows.Forms.TextBox();
             this.pbCalendar = new System.Windows.Forms.PictureBox();
             this.pbSettings = new System.Windows.Forms.PictureBox();
+            this.datePeack = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.pbCalendar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbSettings)).BeginInit();
             this.SuspendLayout();
@@ -40,10 +41,10 @@
             // 
             this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnConnect.Location = new System.Drawing.Point(9, 14);
+            this.btnConnect.Location = new System.Drawing.Point(13, 12);
             this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(87, 58);
+            this.btnConnect.Size = new System.Drawing.Size(87, 72);
             this.btnConnect.TabIndex = 1;
             this.btnConnect.Text = "Get Log";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -53,22 +54,22 @@
             // 
             this.txbLog.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.txbLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txbLog.Location = new System.Drawing.Point(0, 84);
+            this.txbLog.Location = new System.Drawing.Point(0, 104);
             this.txbLog.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txbLog.Multiline = true;
             this.txbLog.Name = "txbLog";
             this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txbLog.Size = new System.Drawing.Size(464, 216);
+            this.txbLog.Size = new System.Drawing.Size(571, 196);
             this.txbLog.TabIndex = 2;
             // 
             // pbCalendar
             // 
             this.pbCalendar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pbCalendar.Image = global::C3_Logger.Properties.Resources.calendar;
-            this.pbCalendar.Location = new System.Drawing.Point(104, 14);
+            this.pbCalendar.Location = new System.Drawing.Point(397, 12);
             this.pbCalendar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pbCalendar.Name = "pbCalendar";
-            this.pbCalendar.Size = new System.Drawing.Size(86, 57);
+            this.pbCalendar.Size = new System.Drawing.Size(86, 72);
             this.pbCalendar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCalendar.TabIndex = 5;
             this.pbCalendar.TabStop = false;
@@ -81,10 +82,10 @@
             this.pbSettings.BackColor = System.Drawing.Color.Transparent;
             this.pbSettings.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pbSettings.Image = global::C3_Logger.Properties.Resources.settings;
-            this.pbSettings.Location = new System.Drawing.Point(361, 14);
+            this.pbSettings.Location = new System.Drawing.Point(525, 12);
             this.pbSettings.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pbSettings.Name = "pbSettings";
-            this.pbSettings.Size = new System.Drawing.Size(86, 57);
+            this.pbSettings.Size = new System.Drawing.Size(33, 34);
             this.pbSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbSettings.TabIndex = 4;
             this.pbSettings.TabStop = false;
@@ -92,11 +93,21 @@
             this.pbSettings.MouseLeave += new System.EventHandler(this.pbSettings_MouseLeave);
             this.pbSettings.MouseHover += new System.EventHandler(this.pbSettings_MouseHover);
             // 
+            // datePeack
+            // 
+            this.datePeack.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePeack.Location = new System.Drawing.Point(226, 12);
+            this.datePeack.Name = "datePeack";
+            this.datePeack.Size = new System.Drawing.Size(115, 21);
+            this.datePeack.TabIndex = 6;
+            this.datePeack.Value = new System.DateTime(2020, 3, 18, 12, 30, 48, 0);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 300);
+            this.ClientSize = new System.Drawing.Size(571, 300);
+            this.Controls.Add(this.datePeack);
             this.Controls.Add(this.pbCalendar);
             this.Controls.Add(this.pbSettings);
             this.Controls.Add(this.txbLog);
@@ -118,6 +129,7 @@
         private System.Windows.Forms.TextBox txbLog;
         private System.Windows.Forms.PictureBox pbSettings;
         private System.Windows.Forms.PictureBox pbCalendar;
+        private System.Windows.Forms.DateTimePicker datePeack;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,25 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.OleDb;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Windows.Forms;
-
-namespace C3_Logger
+﻿namespace C3_Logger
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.OleDb;
+    using System.Drawing;
+    using System.Globalization;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Defines the <see cref="MainForm" />
+    /// </summary>
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Defines the settingsForm
+        /// </summary>
         private SettingsForm settingsForm;
-        String documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\ESD_LOGS\\";
-        string strcount = "";
-        IntPtr h = IntPtr.Zero;
-        Dictionary<int, String> users = new Dictionary<int, String>();
 
+        /// <summary>
+        /// Defines the documents
+        /// </summary>
+        internal String documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\ESD_LOGS\\";
+
+        /// <summary>
+        /// Defines the strcount
+        /// </summary>
+        internal string strcount = "";
+
+        /// <summary>
+        /// Defines the h
+        /// </summary>
+        internal IntPtr h = IntPtr.Zero;
+
+        /// <summary>
+        /// Defines the users
+        /// </summary>
+        internal Dictionary<int, String> users = new Dictionary<int, String>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainForm"/> class.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
@@ -29,50 +53,83 @@ namespace C3_Logger
                 File.Copy(@"Resources\script.js", documents + "script.js");
                 File.Copy(@"Resources\styles.css", documents + "styles.css");
             }
+            getUserNames();
         }
 
+        /// <summary>
+        /// The Connect
+        /// </summary>
+        /// <param name="Parameters">The Parameters<see cref="string"/></param>
+        /// <returns>The <see cref="IntPtr"/></returns>
         [DllImport(@"lib\plcommpro.dll", EntryPoint = "Connect")]
         public static extern IntPtr Connect(string Parameters);
 
+        /// <summary>
+        /// The PullLastError
+        /// </summary>
+        /// <returns>The <see cref="int"/></returns>
         [DllImport(@"lib\plcommpro.dll", EntryPoint = "PullLastError")]
         public static extern int PullLastError();
 
+        /// <summary>
+        /// The Disconnect
+        /// </summary>
+        /// <param name="h">The h<see cref="IntPtr"/></param>
         [DllImport(@"lib\plcommpro.dll", EntryPoint = "Disconnect")]
         public static extern void Disconnect(IntPtr h);
 
+        /// <summary>
+        /// The GetDeviceData
+        /// </summary>
+        /// <param name="h">The h<see cref="IntPtr"/></param>
+        /// <param name="buffer">The buffer<see cref="byte"/></param>
+        /// <param name="buffersize">The buffersize<see cref="int"/></param>
+        /// <param name="tablename">The tablename<see cref="string"/></param>
+        /// <param name="filename">The filename<see cref="string"/></param>
+        /// <param name="filter">The filter<see cref="string"/></param>
+        /// <param name="options">The options<see cref="string"/></param>
+        /// <returns>The <see cref="int"/></returns>
         [DllImport(@"lib\plcommpro.dll", EntryPoint = "GetDeviceData")]
         public static extern int GetDeviceData(IntPtr h, ref byte buffer, int buffersize, string tablename, string filename, string filter, string options);
 
+        /// <summary>
+        /// The btnConnect_Click
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void btnConnect_Click(object sender, EventArgs e)
         {
             Cursor = Cursors.WaitCursor;
             string str = "";
             str = "protocol=TCP,ipaddress=" + Properties.Settings.Default.IP +
-                ",port="+Properties.Settings.Default.Port +
-                ",timeout=3000,passwd="+Properties.Settings.Default.Pass;  //protocol=TCP,ipaddress=192.168.2.46,port=4370,timeout=2000,passwd=
+                ",port=" + Properties.Settings.Default.Port +
+                ",timeout=3000,passwd=" + Properties.Settings.Default.Pass;  //protocol=TCP,ipaddress=192.168.2.46,port=4370,timeout=2000,passwd=
             if (IntPtr.Zero == h)
             {
                 h = Connect(str);
-                
+
                 if (h != IntPtr.Zero)
                 {
                     btnConnect.Enabled = false;
-                    txbLog.Text += "Connected!"+ Environment.NewLine;
+                    txbLog.Text += "Connected!" + Environment.NewLine;
                     getLog();
                     Disconnect(h);
                     h = IntPtr.Zero;
-                    txbLog.Text += "Disconnected!"+ Environment.NewLine;
+                    txbLog.Text += "Disconnected!" + Environment.NewLine;
                     btnConnect.Enabled = true;
                     System.Diagnostics.Process.Start(documents);
                 }
                 else
                 {
-                    txbLog.Text += "Connection error! Cant conncet to address "+ Properties.Settings.Default.IP+":"+ Properties.Settings.Default.Port+ Environment.NewLine;
+                    txbLog.Text += "Connection error! Cant conncet to address " + Properties.Settings.Default.IP + ":" + Properties.Settings.Default.Port + Environment.NewLine;
                 }
                 Cursor = Cursors.Default;
             }
         }
 
+        /// <summary>
+        /// The getLog
+        /// </summary>
         private void getLog()
         {
             int ret = 0;
@@ -106,7 +163,7 @@ namespace C3_Logger
                     txbLog.Text += ex.Message + Environment.NewLine;
                 }
                 saveLog();
-                txbLog.Text +="Get " + ret + " records" + Environment.NewLine;
+                txbLog.Text += "Get " + ret + " records" + Environment.NewLine;
             }
             else
             {
@@ -115,15 +172,20 @@ namespace C3_Logger
             }
         }
 
+        /// <summary>
+        /// The formatLog
+        /// </summary>
+        /// <param name="rowTime">The rowTime<see cref="String"/></param>
+        /// <returns>The <see cref="String[]"/></returns>
         private String[] formatLog(String rowTime)
         {
             int second, minute, hour;
             String strSecond, strMinute, strHour;
-            String[] date =new String[4]; //time,day,month,year
+            String[] date = new String[4]; //time,day,month,year
             int time = Int32.Parse(rowTime);
             second = time % 60;
             if (second < 10)
-                strSecond = "0"+second;
+                strSecond = "0" + second;
             else
                 strSecond = "" + second;
             time = time / 60;
@@ -142,24 +204,26 @@ namespace C3_Logger
             date[0] = String.Format("{0}:{1}:{2}", strHour, strMinute, strSecond);
 
             time = time / 24;
-            date[1] = (time % 31 + 1)+"";
+            date[1] = (time % 31 + 1) + "";
 
             time = time / 31;
-            date[2] = (time % 12 + 1)+"";
+            date[2] = (time % 12 + 1) + "";
 
             time = time / 12;
-            date[3] = (time + 2000)+"";
+            date[3] = (time + 2000) + "";
             return date;
         }
 
-
+        /// <summary>
+        /// The saveLog
+        /// </summary>
         private void saveLog()
         {
             try
             {
                 getUserNames();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 txbLog.Text += ex.Message + Environment.NewLine;
             }
@@ -168,9 +232,9 @@ namespace C3_Logger
             StreamReader sr = new StreamReader(documents + "\\ESD_LOG.ROW");
             try
             {
-                
+
                 StreamWriter sw = new StreamWriter(documents + "\\ESD_LOG.csv");
-                sw.WriteLine("Name,"+sr.ReadLine()+",Day,Month,Year");
+                sw.WriteLine("Name," + sr.ReadLine() + ",Day,Month,Year");
                 while (sr.Peek() > 0)
                 {
                     String[] arr = sr.ReadLine().Split(',');
@@ -178,7 +242,7 @@ namespace C3_Logger
                     String line = "";
                     if (users.Count > 0)
                     {
-                        line = users[Int16.Parse(arr[1])] + "," + arr[0] + "," + arr[1] + "," + arr[2] + ",PASSED," + date[0]+","+ date[1] + "," + date[2] + "," + date[3];
+                        line = users[Int16.Parse(arr[1])] + "," + arr[0] + "," + arr[1] + "," + arr[2] + ",PASSED," + date[0] + "," + date[1] + "," + date[2] + "," + date[3];
                     }
                     else
                     {
@@ -188,7 +252,7 @@ namespace C3_Logger
                     sw.WriteLine(line);
                 }
                 sw.Close();
-                txbLog.Text += "Log created: "+documents + "\\ESD_LOG.csv" + Environment.NewLine;
+                txbLog.Text += "Log created: " + documents + "\\ESD_LOG.csv" + Environment.NewLine;
             }
             catch (Exception ex)
             {
@@ -199,6 +263,9 @@ namespace C3_Logger
             delRow();
         }
 
+        /// <summary>
+        /// The delRow
+        /// </summary>
         private void delRow()
         {
             try
@@ -218,12 +285,17 @@ namespace C3_Logger
             }
         }
 
+        /// <summary>
+        /// The buildHTML
+        /// </summary>
+        /// <param name="year">The year<see cref="int"/></param>
+        /// <param name="month">The month<see cref="int"/></param>
         private void buildHTML(int year, int month)
         {
-            DateTimeFormatInfo dinfo = new DateTimeFormatInfo();
             int nodays = DateTime.DaysInMonth(year, month);
             getUserNames();
-            try { 
+            try
+            {
                 StreamReader sr = new StreamReader(Properties.Settings.Default.LogPath);
                 StreamWriter sw = new StreamWriter(documents + year + "_" + month + ".html");
 
@@ -232,27 +304,34 @@ namespace C3_Logger
                     "<meta name=\"description\"content=\"The HTML5 Herald\">\n" +
                     "<meta name=\"author\"content=\"SitePoint\">\n" +
                     "<link rel=\"stylesheet\" href=\"styles.css?v=1.0\"> </head><body>\n" +
-                    "<script src=\"script.js\"></script>";
+                    "<script src=\"script.js\"></script>\n"+
+                    "<h2>ESD tests for month: "+datePeack.Value.Month+"/"+datePeack.Value.Year+"</h2>";
 
                 sw.WriteLine(page_start);
                 String table = "<table id='tbMonth' border='1'>";
                 String[] headLine = sr.ReadLine().Split(',');
                 table += "<tr><th>ID</th> <th>User Name</th>";
-                for(int i = 1; i <= nodays; i++)
+                for (int i = 1; i <= nodays; i++)
                 {
-                    table += "<th>"+ i + "</th>";
+                    table += "<th>" + i + "</th>";
                 }
-               
+
                 table += "</tr>";
 
-                foreach (KeyValuePair<int, string> user in users)
+                var test = getUsersStats(year, month);
+                for (int i = 1; i < test.GetLength(0); i++)
                 {
-                    table += string.Format("<tr><td>{0}</td><td>{1}</td></tr>", user.Key, user.Value);
+                    table += string.Format("<tr><td>{0}</td>", i);
+                    for (int j = 0; j < test.GetLength(1); j++)
+                    {
+                        table += string.Format("<td>{0}</td>", test[i, j]);
+                    }
+                    table += "</tr>";
                 }
 
                 sw.WriteLine(table);
 
-                String page_end="</body></html>";
+                String page_end = "</body></html>";
                 sw.WriteLine(page_end);
                 sr.Close();
                 sw.Close();
@@ -269,6 +348,9 @@ namespace C3_Logger
             }
         }
 
+        /// <summary>
+        /// The getUserNames
+        /// </summary>
         private void getUserNames()
         {
             string mdfFile = Properties.Settings.Default.dbPath;
@@ -296,42 +378,111 @@ namespace C3_Logger
                         {
 
                         }
-                        
+
                     }
                 }
             }
         }
 
+        private String[,] getUsersStats(int year, int month)
+        {
+            int monthDays = DateTime.DaysInMonth(year, month);
+            String[,] monthStats = new string[users.Count+1, monthDays+1];
+
+            
+
+            if (File.Exists(documents + "\\ESD_LOG.csv"))
+            {
+                foreach (var user in users)
+                {
+                    StreamReader sr = new StreamReader(documents + "\\ESD_LOG.csv");
+                    String[] tmp = sr.ReadLine().Split(',');
+                    monthStats[user.Key, 0] = user.Value;
+                    for(int i = 0; i < monthDays; i++)
+                    {
+                        while(sr.Peek() > 0)
+                        {
+                            tmp = sr.ReadLine().Split(',');
+                            if(Int16.Parse(tmp[7]) == month && Int16.Parse(tmp[8]) == year && user.Value.Equals(tmp[0]))
+                            {
+                                monthStats[user.Key, Int16.Parse(tmp[6])] = " V ";
+                            }
+                        }
+                    }
+                    sr.Close();
+                }
+
+
+
+
+            }
+            else
+            {
+                txbLog.Text += "File not found" + Environment.NewLine;
+            }
+            return monthStats;
+        }
+
+        /// <summary>
+        /// The pbSettings_MouseHover
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void pbSettings_MouseHover(object sender, EventArgs e)
         {
             Cursor = Cursors.Hand;
             pbSettings.BackColor = Color.Gray;
         }
 
+        /// <summary>
+        /// The pbSettings_MouseLeave
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void pbSettings_MouseLeave(object sender, EventArgs e)
         {
             Cursor = Cursors.Default;
             pbSettings.BackColor = Color.Transparent;
-
         }
 
+        /// <summary>
+        /// The pbSettings_Click
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void pbSettings_Click(object sender, EventArgs e)
         {
             settingsForm = new SettingsForm();
             settingsForm.Show();
         }
 
+        /// <summary>
+        /// The pbCalendar_Click
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void pbCalendar_Click(object sender, EventArgs e)
         {
-            buildHTML(2020, 03);
+
+            buildHTML(datePeack.Value.Year, datePeack.Value.Month);
         }
 
+        /// <summary>
+        /// The pbCalendar_MouseHover
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void pbCalendar_MouseHover(object sender, EventArgs e)
         {
             Cursor = Cursors.Hand;
             pbCalendar.BackColor = Color.Gray;
         }
 
+        /// <summary>
+        /// The pbCalendar_MouseLeave
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
         private void pbCalendar_MouseLeave(object sender, EventArgs e)
         {
             Cursor = Cursors.Default;
